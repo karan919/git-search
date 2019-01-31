6c41e6ca9d21eb124cfb9f9d10de9656e443efc2
Karan Mahajan <karanmahajan919@gmail.com>
2019-01-31 14:15:03 +0530

    comment added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; //comment added
 import Header from "./shared/component/Header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
