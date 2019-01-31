c5dfb446a8c010e70cadbedb3f7492b2f7ef73d7
Karan Mahajan <karanmahajan919@gmail.com>
2019-01-31 18:01:38 +0530

    reducer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import store from "./Store";
+import { Provider } from "react-redux";
+
 import Header from "./shared/component/Header";
 import Search from "./component/Search";
 import Contact from "./component/Contact";
@@ -10,18 +13,20 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Search} />
-              <Route exact path="/contact" component={Contact} />
-              <Route component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Search} />
+                <Route exact path="/contact" component={Contact} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
